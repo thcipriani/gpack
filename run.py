1f2c09f2f715800018a03cf43d002e007b30d247
Mukunda Modell <mmodell@wikimedia.org>
2016-02-29 09:27:28 -0600

    rework command line args for run.py
    now we have:
    * --listen for ip address
    * --port, optional
    * --directory, for the path to repositories (e.g. /srv/deployment)

--- run.py
+++ run.py
@@ -2,32 +2,39 @@
 
 import argparse
 import os
+import sys
 
 try:
     from gevent.monkey import patch_all
     patch_all(subprocess=False, aggressive=False)
     from gevent.pywsgi import WSGIServer
 except ImportError:
-    print 'You need install gevent manually! System shutdown.'
+    print 'You need install gevent!'
     sys.exit()
 
 from ghttp import GHTTPServer
 
+
 def parse_args():
     ap = argparse.ArgumentParser()
-    ap.add_argument('-p', '--path', required=True, help='Path from which to serve git')
+    ap.add_argument('-l', '--listen', required=False, default='0.0.0.0',
+                    help='Listen for connections on this IP (default=0.0.0.0)')
+    ap.add_argument('-p', '--port', required=False, default=8080, type=int,
+                    help='Port from which to listen. (default=8080)')
+    ap.add_argument('-d', '--directory', required=True,
+                    help='Directory from which to serve git repos.')
     return ap.parse_args()
 
 
 def main():
     args = parse_args()
-    if not os.path.isdir(args.path):
+    if not os.path.isdir(args.directory):
         raise IOError(2, 'Ain\'t no directory', args.path)
 
-    server = WSGIServer(('0.0.0.0', 8080), GHTTPServer({
+    server = WSGIServer((args.listen, args.port), GHTTPServer({
         'upload_pack': True,
         'receive_pack': True,
-        'project_root': args.path,
+        'project_root': args.directory,
     }))
     server.serve_forever()
 
